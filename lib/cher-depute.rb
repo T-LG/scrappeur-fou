7bddaa9f8b352f96d2848e573ec860f3a9c0a2dc
samielatri <sami.elatri@yahoo.com>
2020-07-16 19:38:58 +0200

    brouillon p3

--- lib/cher-depute.rb
+++ lib/cher-depute.rb
@@ -2,7 +2,36 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-# Retourne la page à scrapper
-def get_page(url_page)
-  return Nokogiri::HTML(open(url_page))
-end 
+def get_res(url)
+  page = Nokogiri::HTML(open(url))
+  res = Hash.new
+  name = page.xpath('/html/body/div[1]/div[3]/div/div/div/section[1]/div/article/div[2]/h1')
+  name_array = name.text.split(' ')
+  email = page.xpath('/html/body/div[1]/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[2]/a')
+  res['first_name'] = name_array[1]
+  res['last_name'] = name_array[2]
+  res['email'] = email.text
+  return res
+end
+
+def get_urls
+  page = Nokogiri::HTML(open())
+  # récupération des liens
+  puts "Start scraping..."
+  urls = page.xpath('//*[@id="deputes-list"]//li/a')
+  return urls
+end
+
+def perform
+  source_url = 
+  results = []
+  # récupération des données dans un tableau
+  get_urls.each do |link|
+    url = source_url + link['href']
+    results << get_res(url)
+  end
+  return results
+end
+
+# P.P
+perform
